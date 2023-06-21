fb3f0707ce611191d4350dd82cd187beb56bfed1
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-21 19:34:59 +0300

    Add login, register and / routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,19 @@
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./features/authentication/Login"
+import Register from "./features/authentication/Register"
+import Home from "./features/shared/Home"
+import Navigation from "./features/shared/Navigation"
 
 function App() {
   return (
-    <div className="App bg-gray-800">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
