108e4369f423c1c23f06be5ea6fc2de099f82386
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-22 03:43:24 +0300

    Update authentication and task reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
-import counterReducer from "../features/counter/counterSlice"
+import authenticationReducer from "../features/authentication/authenticationSlice"
+import taskReducer from "../features/task/taskSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    authentication: authenticationReducer,
+    task: taskReducer,
   },
 })
 
